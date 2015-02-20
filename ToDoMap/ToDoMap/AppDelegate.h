5b7aa6794585f019c03a081888e3aa27ac268503
Ravi Vooda <r.vooda@gmail.com>
2015-02-20 08:27:50 -0800

    Updating gitignore

---
+++ ToDoMap/ToDoMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoMap
+//
+//  Created by Ravi Vooda on 2/20/15.
+//  Copyright (c) 2015 Ravi Vooda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
