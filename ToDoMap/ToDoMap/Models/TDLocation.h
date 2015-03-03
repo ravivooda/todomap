dbd3330e2352ad07552169ca9202c76ca78241b2
Ravi Vooda <r.vooda@gmail.com>
2015-03-02 22:03:39 -0800

    Adding realm

---
+++ ToDoMap/ToDoMap/Models/TDLocation.h
@@ -0,0 +1,13 @@
+//
+//  TDLocation.h
+//  ToDoMap
+//
+//  Created by Ravi Vooda on 2/26/15.
+//  Copyright (c) 2015 Ravi Vooda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDLocation : NSObject
+
+@end
---
+++ ToDoMap/ToDoMap/Models/TDLocation.m
@@ -0,0 +1,13 @@
+//
+//  TDLocation.m
+//  ToDoMap
+//
+//  Created by Ravi Vooda on 2/26/15.
+//  Copyright (c) 2015 Ravi Vooda. All rights reserved.
+//
+
+#import "TDLocation.h"
+
+@implementation TDLocation
+
+@end
